f015ae8557762b02e92215cc881af2c94990f5f8
hugocruzlfc <hugocruzlfc@gmail.com>
2024-02-28 11:01:21 +0100

    feat: adding index

--- src/coffees/entities/event.entity.ts
+++ src/coffees/entities/event.entity.ts
@@ -5,7 +5,7 @@ export class Event extends mongoose.Document {
   @Prop()
   type: string;
 
-  @Prop()
+  @Prop({ index: true })
   name: string;
 
   @Prop(mongoose.SchemaTypes.Mixed)
@@ -13,3 +13,12 @@ export class Event extends mongoose.Document {
 }
 
 export const EventSchema = SchemaFactory.createForClass(Event);
+EventSchema.index({ name: 1, type: -1 });
+/**
+ * In this example:
+ * We passed a value of 1 (to name) which specifies that the index
+ * should order these items in an Ascending order.
+ *
+ * We passed type a value of (negative) -1 which specifies that
+ * The index should order these items in Descending order.
+ */
